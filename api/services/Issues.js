af0eb45b0432cd91a6f9953d4407b38d5085b4e0
Reinier <reinier.guerra@salesforce.com>
2018-11-01 14:40:37 -0700

    fix type create issue

--- api/services/Issues.js
+++ api/services/Issues.js
@@ -7,16 +7,16 @@ module.exports = {
     async create(data) {
         const item = pick(data, [
             'subject',
-            'decription',
+            'description',
             'productTag',
             'status',
         ]);
         return await Issues.create(item).fetch();
     },
     async get(id) {
-        return await Issues.findOne({ id });
+        return await Issues.findOne({id});
     },
     async update(id, issue) {
-        return await Issues.update({ id }, issue).fetch();
+        return await Issues.update({id}, issue).fetch();
     }
-}
+};
