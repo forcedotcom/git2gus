f6dff34d5eeb0ce5533973a8e92faccc048c1ff1
reiniergs <reiniergs@gmail.com>
2019-02-14 16:17:50 -0500

    test: add updateGusItemTitle tests

--- api/hooks/issues-hook/update-gus-item-title/index.js
+++ api/hooks/issues-hook/update-gus-item-title/index.js
@@ -1,29 +1,9 @@
 const Issues = require('../../../services/Issues');
-const Logger = require('../../../services/Logger');
 
 module.exports = async function updateGusItemTitle({ subject, relatedUrl }) {
     const issue = await Issues.getByRelatedUrl(relatedUrl);
     if (issue && Issues.weCreateIssue(issue)) {
-        Logger.log({
-            message: `Updating GUS item title to: ${subject}`,
-            event: {
-                update_gus_item: {
-                    id: issue.id,
-                    subject,
-                },
-            }
-        });
         return Issues.update(issue.id, { subject });
     }
-    Logger.log({
-        type: 'error',
-        message: `There isn't a GUS item related found or it wasn't created by us`,
-        event: {
-            context: {
-                gusUserId: sails.config.gus.gusUserId,
-                issue,
-            }
-        },
-    });
     return null;
 };
