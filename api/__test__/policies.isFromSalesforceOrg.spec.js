2499f197d9f08b2aa466c4a8ff3e1358374e5ce2
Reinier Guerra <reinier.guerra@salesforce.com>
2019-06-13 18:54:31 -0400

    fix: test in wrong place

--- api/__test__/policies.isFromSalesforceOrg.spec.js
+++ api/__test__/policies.isFromSalesforceOrg.spec.js
@@ -94,4 +94,58 @@ describe('isFromSalesforceOrg policy', () => {
             message: 'The request received is not from salesforce org.'
         });
     });
+    it('should allows salesforce org installations', () => {
+        const req = {
+            body: {
+                installation: { account: { login: 'Sfdc' } }
+            },
+            headers: {
+                'x-github-event': 'installation'
+            }
+        };
+        const next = jest.fn();
+        isFromSalesforceOrg(req, {}, next);
+        expect(next).toHaveBeenCalledTimes(1);
+    });
+    it('should allows regular event from salesforce orgs', () => {
+        const req = {
+            body: {
+                repository: { owner: { login: 'salesforce' } }
+            },
+            headers: {
+                'x-github-event': 'foo'
+            }
+        };
+        const next = jest.fn();
+        isFromSalesforceOrg(req, {}, next);
+        expect(next).toHaveBeenCalledTimes(1);
+    });
+    it('should block event from non-salesforce orgs', () => {
+        const req = {
+            body: {
+                repository: { owner: { login: 'reiniergs' } }
+            },
+            headers: {
+                'x-github-event': 'foo'
+            }
+        };
+        const next = jest.fn();
+        const res = { badRequest: jest.fn() };
+        isFromSalesforceOrg(req, res, next);
+        expect(res.badRequest).toHaveBeenCalledTimes(1);
+    });
+    it('should block org that contains salesforce in the name', () => {
+        const req = {
+            body: {
+                repository: { owner: { login: 'salesforce-fake' } }
+            },
+            headers: {
+                'x-github-event': 'foo'
+            }
+        };
+        const next = jest.fn();
+        const res = { badRequest: jest.fn() };
+        isFromSalesforceOrg(req, res, next);
+        expect(res.badRequest).toHaveBeenCalledTimes(1);
+    });
 });
