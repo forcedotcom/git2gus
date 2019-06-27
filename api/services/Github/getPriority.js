4fd2ca4b44af639e26c312abbcb60dc3f1227b3f
Bryan Lim <bryanlim@cs.washington.edu>
2019-06-27 15:39:56 -0700

    Utilities to get priority of investigation
    Added to the priority utilities for bugs

--- api/services/Github/getPriority.js
+++ api/services/Github/getPriority.js
@@ -1,4 +1,5 @@
 const isGusBugLabel = require('./isGusBugLabel');
+const isGusInvestigationLabel = require('./isGusInvestigationLabel');
 
 module.exports = function getPriority(labels) {
     let priority;
@@ -8,6 +9,11 @@ module.exports = function getPriority(labels) {
             (priority === undefined || name[5] < priority[1])
         ) {
             priority = `P${name[5]}`;
+        } else if (
+            isGusInvestigationLabel(name) &&
+            (priority === undefined || name[20] < priority[1])
+        ) {
+            priority = `P${name[20]}`;
         }
     });
     return priority;
