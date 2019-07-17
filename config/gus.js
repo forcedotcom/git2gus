3e132baca1f879abab4e095de387d9f6263cb272
Bryan Lim <bryanlim@cs.washington.edu>
2019-07-17 11:15:52 -0700

    Move record type IDs to config

--- config/gus.js
+++ config/gus.js
@@ -1,7 +1,7 @@
 module.exports.gus = {
     gusUserId: process.env.GUS_USER_ID,
     status: ['INTEGRATE', 'FIXED', 'CLOSED'],
-    userStoryRecordTypeId: '0129000000006gDAAQ',
+    userStoryRecordTypeId: process.env.USER_STORY_RECORD_TYPE_ID,
     /* Note: bugRecordTypeId is adm_work__c default record type */
-    bugRecordTypeId: '012T00000004MUHIA2'
+    bugRecordTypeId: process.env.BUG_RECORD_TYPE_ID
 };
