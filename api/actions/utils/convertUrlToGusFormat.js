2597f7cb82617db9a641aee48571615f8c0d2fcb
Jag Jayaprakash <58611665+jag-sfdc@users.noreply.github.com>
2020-07-10 10:00:26 -0700

    Changelist URL bug fix - GUS expects the only relative Github url. Changing absolute URL to only pathname (#77)

--- api/actions/utils/convertUrlToGusFormat.js
+++ api/actions/utils/convertUrlToGusFormat.js
@@ -4,11 +4,15 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+const URL = require('url');
 
 function convertUrlToGusFormat(repo_url, merge_commit_sha, pr_url) {
     if (merge_commit_sha) {
-        return repo_url.concat('/commit/').concat(merge_commit_sha);
+        return URL.parse(repo_url)
+            .pathname.concat('/commit/')
+            .concat(merge_commit_sha)
+            .substr(1);
     }
-    return pr_url;
+    return URL.parse(pr_url).pathname.substr(1);
 }
 exports.convertUrlToGusFormat = convertUrlToGusFormat;
