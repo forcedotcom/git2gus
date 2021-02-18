64771805cff59e4738081ceb89514a622e213342
Jag <jjayaprakash@salesforce.com>
2021-02-18 10:34:10 -0800

    fix: Fixing the duplicate story creation issue and not used connect api code

--- api/actions/__test__/createWorkItem.spec.js
+++ api/actions/__test__/createWorkItem.spec.js
@@ -7,8 +7,9 @@
 
 const { fn } = require('../createWorkItem');
 const Builds = require('../../services/Builds');
+const formatToGus = require('../../actions/formatToGus');
 const Github = require('../../services/Github');
-const { getWorkItemUrl, waitUntilSynced } = require('./../../services/Issues');
+const Gus = require('../../services/Gus');
 
 jest.mock('../../services/Builds', () => ({
     resolveBuild: jest.fn()
@@ -16,13 +17,23 @@ jest.mock('../../services/Builds', () => ({
 jest.mock('../../services/Github', () => ({
     isSalesforceLabel: jest.fn(),
     getPriority: () => 'P1',
-    getRecordTypeId: () => 'bug',
+    getRecordTypeId: jest.fn(),
     createComment: jest.fn(),
     updateDescription: jest.fn()
 }));
 jest.mock('../../services/Issues', () => ({
     getWorkItemUrl: jest.fn(),
     waitUntilSynced: jest.fn()
+}));
+jest.mock('../../services/Gus', () => ({
+    getByRelatedUrl: jest.fn(),
+    createWorkItemInGus: jest.fn(),
+    resolveBuild: jest.fn(),
+    getBugRecordTypeId: jest.fn(),
+    getById: jest.fn()
+}));
+jest.mock('../../actions/formatToGus', () => ({
+    formatToGus: jest.fn()
 }));
 
 global.sails = {
@@ -34,6 +45,10 @@ global.sails = {
         }
     }
 };
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
 
 const req = {
     body: {
@@ -165,6 +180,39 @@ const reqWithIssueTypeLabels = {
     }
 };
 
+const reqWithIssueTypeUserStory = {
+    body: {
+        issue: {
+            html_url: 'github/git2gus-test/#30',
+            title: 'new feature request',
+            body: 'some description',
+            number: 30,
+            labels: [{ name: 'feature' }, { name: 'notAValidLabel' }]
+        },
+        label: 'feature',
+        repository: {
+            name: 'git2gus-test',
+            owner: {
+                login: 'john'
+            }
+        }
+    },
+    git2gus: {
+        config: {
+            issueTypeLabels: {
+                feature: 'USER STORY'
+            },
+            productTag: 'abcd1234',
+            defaultBuild: '218'
+        }
+    },
+    octokitClient: {
+        issues: {
+            createComment: jest.fn()
+        }
+    }
+};
+
 const reqWithGusTitlePrefix = {
     body: {
         issue: {
@@ -197,219 +245,153 @@ const reqWithGusTitlePrefix = {
 };
 
 describe('createGusItem action', () => {
-    it('should call queue push with formatted text', async () => {
+    it('should call createWorkItemInGus with formatted text', async () => {
         expect.assertions(1);
+        Github.getRecordTypeId.mockReturnValue('bug');
         Github.isSalesforceLabel.mockReturnValue(true);
         Builds.resolveBuild.mockReturnValue(Promise.resolve('qwerty1234'));
+        formatToGus.formatToGus.mockReturnValue(Promise.resolve('Body In Gus format'));
+        Gus.resolveBuild.mockReturnValue(Promise.resolve('229'));
+        Gus.getByRelatedUrl.mockReturnValue(Promise.resolve(''));
+        Gus.getBugRecordTypeId.mockReturnValue(Promise.resolve('bug'));
         await fn(req);
-        expect(sails.hooks['issues-hook'].queue.push).toHaveBeenCalledWith(
-            {
-                name: 'CREATE_WORK_ITEM',
-                subject: 'new issue',
-                description:
-                    'Github issue link: github/git2gus-test/#30\n<hr><hr><h3>some title</h3>\n<p>some description</p>\n',
-                storyDetails:
-                    'Github issue link: github/git2gus-test/#30\n<hr><hr><h3>some title</h3>\n<p>some description</p>\n',
-                productTag: 'abcd1234',
-                status: 'NEW',
-                foundInBuild: 'qwerty1234',
-                priority: 'P1',
-                relatedUrl: 'github/git2gus-test/#30',
-                recordTypeId: 'bug'
-            },
-            expect.any(Function)
-        );
+        expect(Gus.createWorkItemInGus).toHaveBeenCalledWith('new issue', "Body In Gus format", "abcd1234", "NEW", "229", "P1", "github/git2gus-test/#30", "bug");
     });
-    it('should call queue push with the right title prefix text', async () => {
+    it('should call createWorkItemInGus with the right title prefix text', async () => {
         expect.assertions(1);
+        Github.getRecordTypeId.mockReturnValue('bug');
         Github.isSalesforceLabel.mockReturnValue(true);
         Builds.resolveBuild.mockReturnValue(Promise.resolve('qwerty1234'));
+        formatToGus.formatToGus.mockReturnValue(Promise.resolve('Body In Gus format'));
+        Gus.resolveBuild.mockReturnValue(Promise.resolve('229'));
+        Gus.getByRelatedUrl.mockReturnValue(Promise.resolve(''));
+        Gus.getBugRecordTypeId.mockReturnValue(Promise.resolve('bug'));
         await fn(reqWithGusTitlePrefix);
-        expect(sails.hooks['issues-hook'].queue.push).toHaveBeenCalledWith(
-            {
-                name: 'CREATE_WORK_ITEM',
-                subject: '[Some Prefix Text]'.concat(' new issue'),
-                description:
-                    'Github issue link: github/git2gus-test/#30\n<hr><hr><p>some description</p>\n',
-                storyDetails:
-                    'Github issue link: github/git2gus-test/#30\n<hr><hr><p>some description</p>\n',
-                productTag: 'abcd1234',
-                status: 'NEW',
-                foundInBuild: 'qwerty1234',
-                priority: 'P1',
-                relatedUrl: 'github/git2gus-test/#30',
-                recordTypeId: 'bug'
-            },
-            expect.any(Function)
-        );
+        expect(Gus.createWorkItemInGus).toHaveBeenCalledWith('[Some Prefix Text]'.concat(' new issue'), "Body In Gus format", "abcd1234", "NEW", "229", "P1", "github/git2gus-test/#30", "bug");
     });
-    it('should call queue push with the right issue type from label', async () => {
+    it('should call createWorkItemInGus with the right issue type from label', async () => {
         expect.assertions(1);
+        Github.getRecordTypeId.mockReturnValue('bug');
         Github.isSalesforceLabel.mockReturnValue(true);
         Builds.resolveBuild.mockReturnValue(Promise.resolve('qwerty1234'));
+        formatToGus.formatToGus.mockReturnValue(Promise.resolve('Body In Gus format'));
+        Gus.resolveBuild.mockReturnValue(Promise.resolve('229'));
+        Gus.getByRelatedUrl.mockReturnValue(Promise.resolve(''));
+        Gus.getBugRecordTypeId.mockReturnValue(Promise.resolve('bug'));
         await fn(reqWithIssueTypeLabels);
-        expect(sails.hooks['issues-hook'].queue.push).toHaveBeenCalledWith(
-            {
-                name: 'CREATE_WORK_ITEM',
-                subject: 'new issue',
-                description:
-                    'Github issue link: github/git2gus-test/#30\n<hr><hr><p>some description</p>\n',
-                storyDetails:
-                    'Github issue link: github/git2gus-test/#30\n<hr><hr><p>some description</p>\n',
-                productTag: 'abcd1234',
-                status: 'NEW',
-                foundInBuild: 'qwerty1234',
-                priority: 'P1',
-                relatedUrl: 'github/git2gus-test/#30',
-                recordTypeId: 'bug'
-            },
-            expect.any(Function)
-        );
+        expect(Gus.createWorkItemInGus).toHaveBeenCalledWith('new issue', "Body In Gus format", "abcd1234", "NEW", "229", "P1", "github/git2gus-test/#30", "bug");
     });
-    it('should call queue push with the right product tag label', async () => {
+    it('should call createWorkItemInGus with the right issue type from label', async () => {
         expect.assertions(1);
+        Github.getRecordTypeId.mockReturnValue('bug');
         Github.isSalesforceLabel.mockReturnValue(true);
         Builds.resolveBuild.mockReturnValue(Promise.resolve('qwerty1234'));
+        formatToGus.formatToGus.mockReturnValue(Promise.resolve('Body In Gus format'));
+        Gus.resolveBuild.mockReturnValue(Promise.resolve('229'));
+        Gus.getByRelatedUrl.mockReturnValue(Promise.resolve(''));
+        Gus.getBugRecordTypeId.mockReturnValue(Promise.resolve('bug'));
+        await fn(reqWithIssueTypeLabels);
+        expect(Gus.createWorkItemInGus).toHaveBeenCalledWith('new issue', "Body In Gus format", "abcd1234", "NEW", "229", "P1", "github/git2gus-test/#30", "bug");
+    });
+    it('should call createWorkItemInGus with the right product tag label', async () => {
+        expect.assertions(1);
+        Github.getRecordTypeId.mockReturnValue('bug');
+        Github.isSalesforceLabel.mockReturnValue(true);
+        Builds.resolveBuild.mockReturnValue(Promise.resolve('qwerty1234'));
+        formatToGus.formatToGus.mockReturnValue(Promise.resolve('Body In Gus format'));
+        Gus.resolveBuild.mockReturnValue(Promise.resolve('229'));
+        Gus.getByRelatedUrl.mockReturnValue(Promise.resolve(''));
+        Gus.getBugRecordTypeId.mockReturnValue(Promise.resolve('bug'));
         await fn(reqWithProductTagLabel);
-        expect(sails.hooks['issues-hook'].queue.push).toHaveBeenCalledWith(
-            {
-                name: 'CREATE_WORK_ITEM',
-                subject: 'new issue',
-                description:
-                    'Github issue link: github/git2gus-test/#30\n<hr><hr><p>some description</p>\n',
-                storyDetails:
-                    'Github issue link: github/git2gus-test/#30\n<hr><hr><p>some description</p>\n',
-                productTag: 'efgh5678',
-                status: 'NEW',
-                foundInBuild: 'qwerty1234',
-                priority: 'P1',
-                relatedUrl: 'github/git2gus-test/#30',
-                recordTypeId: 'bug'
-            },
-            expect.any(Function)
-        );
+        expect(Gus.createWorkItemInGus).toHaveBeenCalledWith('new issue', "Body In Gus format", "efgh5678", "NEW", "229", "P1", "github/git2gus-test/#30", "bug");
     });
-    it('should not push to queue if no product tag and label is not a product tag label', async () => {
+    it('should not call createWorkItemInGus if user story already exists in GUS', async () => {
         expect.assertions(1);
-        sails.hooks['issues-hook'].queue.push.mockReset();
+        Github.getRecordTypeId.mockReturnValue('story');
         Github.isSalesforceLabel.mockReturnValue(true);
         Builds.resolveBuild.mockReturnValue(Promise.resolve('qwerty1234'));
+        formatToGus.formatToGus.mockReturnValue(Promise.resolve('Body In Gus format'));
+        Gus.resolveBuild.mockReturnValue(Promise.resolve('229'));
+        Gus.getBugRecordTypeId.mockReturnValue(Promise.resolve('bug'));
+        var dummyIssue = {RecordTypeId : 'story'};
+        Gus.getByRelatedUrl.mockReturnValue(Promise.resolve(dummyIssue));
+        await fn(req);
+        expect(Gus.createWorkItemInGus).not.toHaveBeenCalled();
+    });
+    it('should not call createWorkItemInGus if bug already exists in GUS', async () => {
+        expect.assertions(1);
+        Github.getRecordTypeId.mockReturnValue('bug');
+        Github.isSalesforceLabel.mockReturnValue(true);
+        Builds.resolveBuild.mockReturnValue(Promise.resolve('qwerty1234'));
+        formatToGus.formatToGus.mockReturnValue(Promise.resolve('Body In Gus format'));
+        Gus.resolveBuild.mockReturnValue(Promise.resolve('229'));
+        Gus.getBugRecordTypeId.mockReturnValue(Promise.resolve('bug'));
+        var dummyIssue = {RecordTypeId : 'bug', Priority__c : '1'};
+        Gus.getByRelatedUrl.mockReturnValue(Promise.resolve(dummyIssue));
+        await fn(reqWithIssueTypeUserStory);
+        expect(Gus.createWorkItemInGus).not.toHaveBeenCalled();
+    });
+    it('should not call createWorkItemInGus if no product tag and label is not a product tag label', async () => {
+        expect.assertions(1);
+        Github.getRecordTypeId.mockReturnValue('bug');
+        Github.isSalesforceLabel.mockReturnValue(true);
+        Builds.resolveBuild.mockReturnValue(Promise.resolve('qwerty1234'));
+        formatToGus.formatToGus.mockReturnValue(Promise.resolve('Body In Gus format'));
+        Gus.resolveBuild.mockReturnValue(Promise.resolve('229'));
+        Gus.getByRelatedUrl.mockReturnValue(Promise.resolve(''));
+        Gus.getBugRecordTypeId.mockReturnValue(Promise.resolve('abcd1234'));
         await fn(reqWithOnlyProductTagLabels);
-        expect(sails.hooks['issues-hook'].queue.push).not.toHaveBeenCalled();
+        expect(Gus.createWorkItemInGus).not.toHaveBeenCalled();
     });
     it('should create a github comment when there is not a valid build and milestone', async () => {
-        expect.assertions(2);
-        sails.hooks['issues-hook'].queue.push.mockReset();
+        expect.assertions(1);
+        Github.getRecordTypeId.mockReturnValue('bug');
         Github.isSalesforceLabel.mockReturnValue(true);
+        formatToGus.formatToGus.mockReturnValue(Promise.resolve('Body In Gus format'));
+        Gus.resolveBuild.mockReturnValue(Promise.resolve(null));
+        Gus.getByRelatedUrl.mockReturnValue(Promise.resolve(''));
+        Gus.getBugRecordTypeId.mockReturnValue(Promise.resolve('bug'));
         Github.createComment.mockReset();
-        Builds.resolveBuild.mockReturnValue(Promise.resolve(null));
         await fn(req);
-        expect(Github.createComment).toHaveBeenCalledWith({
-            req,
-            body:
-                "The defaultBuild value 218 in `.git2gus/config.json` doesn't match any valid build in Salesforce."
-        });
-        expect(sails.hooks['issues-hook'].queue.push).not.toHaveBeenCalled();
-    });
-    it('should create a github comment when there is not a valid build but there is a milestone', async () => {
-        expect.assertions(2);
-        sails.hooks['issues-hook'].queue.push.mockReset();
-        Github.isSalesforceLabel.mockReturnValue(true);
-        Github.createComment.mockReset();
-        Builds.resolveBuild.mockReturnValue(Promise.resolve(null));
-        req.body.issue.milestone = {
-            title: 220
-        };
-        await fn(req);
-        expect(Github.createComment).toHaveBeenCalledWith({
-            req,
-            body:
-                "The milestone assigned to the issue doesn't match any valid build in Salesforce."
-        });
-        expect(sails.hooks['issues-hook'].queue.push).not.toHaveBeenCalled();
-    });
-    it('should not call anything when the label is not a Salesforce label', async () => {
-        expect.assertions(2);
-        Github.createComment.mockReset();
-        sails.hooks['issues-hook'].queue.push.mockReset();
-        Github.isSalesforceLabel.mockReturnValue(false);
-        await fn(req);
-        expect(Github.createComment).not.toHaveBeenCalled();
-        expect(sails.hooks['issues-hook'].queue.push).not.toHaveBeenCalled();
+        expect(Github.createComment).toHaveBeenCalledWith({'body': 'Error while creating work item. No valid build found in GUS!', req});
     });
     it('should create a comment when the "done" callback return the new work item and it is synced', async () => {
-        expect.assertions(3);
-        Github.createComment.mockReset();
+        expect.assertions(1);
+        Github.getRecordTypeId.mockReturnValue('bug');
         Github.isSalesforceLabel.mockReturnValue(true);
-        Builds.resolveBuild.mockReturnValue(
-            Promise.resolve({ sfid: 'B12345' })
-        );
-        getWorkItemUrl.mockReset();
-        waitUntilSynced.mockReturnValue(Promise.resolve({ sfci: 'SF123456' }));
-        getWorkItemUrl.mockReturnValue('https://12345.com');
-        sails.hooks['issues-hook'].queue.push = async (data, done) => {
-            done(null, { id: '12345' });
-        };
+        Builds.resolveBuild.mockReturnValue(Promise.resolve('qwerty1234'));
+        formatToGus.formatToGus.mockReturnValue(Promise.resolve('Body In Gus format'));
+        Gus.resolveBuild.mockReturnValue(Promise.resolve('229'));
+        Gus.getByRelatedUrl.mockReturnValue(Promise.resolve(''));
+        Gus.getBugRecordTypeId.mockReturnValue(Promise.resolve('bug'));
+        const workItem = {id : '12345', Name: 'new-work-item'};
+        Gus.createWorkItemInGus.mockReturnValue(workItem);
+        Gus.getById.mockReturnValue(workItem);
         await fn(req);
-        expect(waitUntilSynced).toHaveBeenCalledWith(
-            { id: '12345' },
-            { interval: 60000, times: 5 }
-        );
-        expect(getWorkItemUrl).toHaveBeenCalledWith(
-            { sfci: 'SF123456' },
-            undefined
-        );
         expect(Github.createComment).toHaveBeenCalledWith({
             req,
-            body: `This issue has been linked to a new work item: https://12345.com`
+            body: `This issue has been linked to a new work item: [new-work-item](undefined12345/view)`
         });
     });
 
     it('should create a comment without the url when the git2gus.config.hideWorkItemUrl = true', async () => {
-        expect.assertions(2);
-        Github.createComment.mockReset();
+        expect.assertions(1);
+        Github.getRecordTypeId.mockReturnValue('bug');
         Github.isSalesforceLabel.mockReturnValue(true);
-        Builds.resolveBuild.mockReturnValue(
-            Promise.resolve({ sfid: 'B12345' })
-        );
-        getWorkItemUrl.mockReset();
-        waitUntilSynced.mockReturnValue(Promise.resolve({ sfci: 'SF123456' }));
-        getWorkItemUrl.mockReturnValue('https://12345.com');
-        sails.hooks['issues-hook'].queue.push = async (data, done) => {
-            done(null, { id: '12345' });
-        };
-
+        Builds.resolveBuild.mockReturnValue(Promise.resolve('qwerty1234'));
+        formatToGus.formatToGus.mockReturnValue(Promise.resolve('Body In Gus format'));
+        Gus.resolveBuild.mockReturnValue(Promise.resolve('229'));
+        Gus.getByRelatedUrl.mockReturnValue(Promise.resolve(''));
+        Gus.getBugRecordTypeId.mockReturnValue(Promise.resolve('bug'));
+        const workItem = {id : '12345', Name: 'new-work-item'};
+        Gus.createWorkItemInGus.mockReturnValue(workItem);
+        Gus.getById.mockReturnValue(workItem);
         const req1 = JSON.parse(JSON.stringify(req));
-        req1.git2gus.config.hideWorkItemUrl = true;
-
+        req1.git2gus.config.hideWorkItemUrl = 'true';
         await fn(req1);
-
-        expect(waitUntilSynced).toHaveBeenCalledWith(
-            { id: '12345' },
-            { interval: 60000, times: 5 }
-        );
-        expect(getWorkItemUrl).toHaveBeenCalledWith({ sfci: 'SF123456' }, true);
-    });
-
-    it('should create a comment when the "done" callback return the new work item but it not get synced', async () => {
-        expect.assertions(3);
-        Github.createComment.mockReset();
-        Github.isSalesforceLabel.mockReturnValue(true);
-        Builds.resolveBuild.mockReturnValue(
-            Promise.resolve({ sfid: 'B12345' })
-        );
-        getWorkItemUrl.mockReset();
-        waitUntilSynced.mockReturnValue(Promise.resolve(undefined));
-        await fn(req);
-        expect(waitUntilSynced).toHaveBeenCalledWith(
-            { id: '12345' },
-            { interval: 60000, times: 5 }
-        );
-        expect(getWorkItemUrl).not.toHaveBeenCalled();
         expect(Github.createComment).toHaveBeenCalledWith({
-            req,
-            body:
-                'Sorry we could not wait until Heroku connect make the synchronization.'
+            req: req1,
+            body: `This issue has been linked to a new work item: new-work-item`
         });
     });
 });
