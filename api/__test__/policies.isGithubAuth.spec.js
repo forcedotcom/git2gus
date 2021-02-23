1399f7b6c2fed8a335bdd8c1d948e8047ed10627
Reinier Guerra <reinier.guerra@salesforce.com>
2021-02-22 19:31:57 -0500

    fix: labels creation when install (#107)

--- api/__test__/policies.isGithubAuth.spec.js
+++ api/__test__/policies.isGithubAuth.spec.js
@@ -21,29 +21,11 @@ jest.mock('../../config/github', () => ({
 }));
 
 const testId = '410939550';
-const testOrgName = 'SSOEnabledOrg';
-
-process.env.TOKEN_ORGS = testOrgName;
-process.env.PERSONAL_ACCESS_TOKEN = 'personalAccessToken123';
 
 const req = {
     body: {
         installation: {
             id: testId
-        },
-        repository: {
-            full_name: 'testOrg/testRepo'
-        }
-    }
-};
-
-const SSOReq = {
-    body: {
-        installation: {
-            id: testId
-        },
-        repository: {
-            full_name: testOrgName + '/testRepo'
         }
     }
 };
@@ -68,13 +50,6 @@ describe('isGithubAuth policy', () => {
             auth: expect.any(Function)
         });
     });
-    it('should call Octokit using peronsal access token for SSO Org', () => {
-        Octokit.mockReset();
-        isGithubAuth(SSOReq, res, next);
-        expect(Octokit).toHaveBeenCalledWith({
-            auth: process.env.PERSONAL_ACCESS_TOKEN
-        });
-    });
     it('should attach octokitClient to req', () => {
         Octokit.mockReset();
         Octokit.mockImplementation(() => {
@@ -92,16 +67,12 @@ describe('isGithubAuth policy', () => {
         isGithubAuth(req, res, next);
         expect(next).toHaveBeenCalled();
     });
-    it('should respond with 401 status when getInstallationAccessToken rejects', async () => {
-        App.mockImplementation(() => {
-            return {
-                getInstallationAccessToken: jest.fn(() => Promise.reject())
-            };
+    it('should call Octokit using peronsal access token for SSO Org', () => {
+        Octokit.mockReset();
+        process.env.PERSONAL_ACCESS_TOKEN = 'personalAccessToken123';
+        isGithubAuth(req, res, next);
+        expect(Octokit).toHaveBeenCalledWith({
+            auth: process.env.PERSONAL_ACCESS_TOKEN
         });
-        Octokit.mockImplementation(async ({ auth }) => {
-            return await auth();
-        });
-        await isGithubAuth(req, res, next);
-        expect(res.status).toHaveBeenCalledWith(401);
     });
 });
