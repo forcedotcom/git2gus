51a2600f67ba2bb00829a8dc424a63a85ab2d575
Mike Senn <msenn@salesforce.com>
2023-02-14 10:13:44 -0500

    Update salesforce.js
    Fixes #153

--- config/salesforce.js
+++ config/salesforce.js
@@ -6,7 +6,7 @@
  */
 
 module.exports.salesforce = {
-    salesforceUserId: process.env.SALESFORCE_USER_ID,
+    salesforceUserId: process.env.GUS_USER,
     status: ['INTEGRATE', 'FIXED', 'CLOSED'],
     userStoryRecordTypeId: process.env.USER_STORY_RECORD_TYPE_ID,
     /* Note: bugRecordTypeId is adm_work__c default record type */
