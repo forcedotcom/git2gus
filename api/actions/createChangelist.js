c9bb798a2d4f53bddb502f1d3abe49955516eb35
Jag Jayaprakash <jjayaprakash@salesforce.com>
2024-02-26 09:33:12 -0800

    add additional logs to adding changelists to GUS workitem

--- api/actions/createChangelist.js
+++ api/actions/createChangelist.js
@@ -13,6 +13,7 @@ const logger = require('../services/Logs/logger');
 module.exports = {
     eventName: GithubEvents.events.PULL_REQUEST_CLOSED,
     fn: async function(req) {
+        logger.info('createChangelist() Action called successfully');
         const {
             pull_request: {
                 title,
@@ -28,27 +29,39 @@ module.exports = {
 
         if (!merged_at) {
             logger.info(
-                `Skipping createChangelistInGus because merged_at is null. PR was closed without merging`,
+                `Skipping createChangelistInGus() because merged_at is null. PR was closed without merging`,
                 { pr_url, title }
             );
             return;
         }
+        logger.info(`Extracted merged_at:${merged_at} from the req`);
+
         const workItemInTitleOrBody = title
             .concat(body)
             .match('@[Ww]-\\d{6,8}@');
+        logger.info(`Extracted workItem Id:${workItemInTitleOrBody} from the req`);
+
         if (workItemInTitleOrBody) {
             const workItemName = workItemInTitleOrBody[0].substring(
                 1,
                 workItemInTitleOrBody[0].length - 1
             );
+            logger.info(`Extracted workItemName:${workItemName} from the req`);
 
             const changelistUrl = convertUrlToGusFormat(
                 repo_url,
                 merge_commit_sha,
                 pr_url
             );
-            const issueId = await Gus.getWorkItemIdByName(workItemName);
-            Gus.createChangelistInGus(changelistUrl, issueId, merged_at);
+            logger.info(`changelistUrl to be appended to the GUS workitem: ${changelistUrl}`);
+
+            try {
+                const issueId = await Gus.getWorkItemIdByName(workItemName);
+                logger.info(`Creating changelist in GUS for issueId: ${issueId}, changelistUrl: ${changelistUrl}, merged_at: ${merged_at}`);
+                Gus.createChangelistInGus(changelistUrl, issueId, merged_at);
+            } catch (error) {
+                logger.error(`createChangelistInGus() errored: ${error}`);
+            }
         }
     }
 };
